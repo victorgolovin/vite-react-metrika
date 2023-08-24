517bd6d28a53c075e0e6bc59ee1e039594fe860c
Victor Golovin <rinoally@gmail.com>
2023-08-24 22:36:51 +0300

    add commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AboutSection from './components/AboutSection/AboutSection'
+import HeroSection from './components/HeroSection/HeroSection'
+import ProjectsSection from './components/ProjectsSection/ProjectsSection'
+
+const App = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <h1>Современные и стильные интерьеры для комфортной жизни</h1>
+  )
+}
+
+export default Heading
